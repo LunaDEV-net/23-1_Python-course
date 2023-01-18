5016047f6f57339df76f961efb8254dd56684652
lunaDEV <lunadev@posteo.net>
2023-01-18 19:01:40 +0100

    fin day one

--- turtle/1/speedtest.py
+++ turtle/1/speedtest.py
@@ -1,12 +1,18 @@
 import turtle as t
 t.penup()
-t.goto(50,50)
 
-for i in range(1, 5):
-    t.pendown()
-    t.right(90)
-    t.forward(100)
-    print(i)
-    t.penup()
-t.home()
+t.goto(50,50)
+def makeSquare(fow=100):
+    for i in range(1, 5):
+        t.pendown()
+        t.right(90)
+        t.forward(100)
+        print(i)
+        t.penup()
+    #t.home()
+
+def makeStar():
+    t.pen()
+
+#makeSquare()
 input("")
